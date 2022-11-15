0b15a4ec586169cfdd2d179c06c693416dbb89cd
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2022-11-15 15:01:42 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com?apikey=34b16bc1'
+
+const movie1 = {
+    "Title": "The Prestige",
+    "Year": "2006",
+    "imdbID": "tt0482571",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjA4NDI0MTIxNF5BMl5BanBnXkFtZTYwNTM0MzY2._V1_SX300.jpg"
+}
+
+const App = () =>{
+    const [movies, setmovies] = useState("");
+    const [searchTerm, setsearchTerm] = useState([])
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setmovies(data.Search);
+    }
+    useEffect(()=>{
+        searchMovies('The Prestige');
+    },[]);
+
+    return(
+        <div className='app' >
+            <h1>Procastination Assistant</h1>
+            <div className='search' >
+                <input
+                placeholder='Movie name'
+                value={searchTerm}
+                onChange={(e)=> setsearchTerm(e.target.value) }
+                />
+            <img
+            src={SearchIcon}
+            alt='search'
+            onClick={()=>searchMovies(searchTerm)}
+            />
+            </div>
+            {movies?.length >0 ?
+                (
+                    <div className='container'>
+                    {movies.map((movie)=>(
+                        <MovieCard movie={movie} />
+                    ))}
+                    </div>
+                ) : (
+                    <div className='empty' >
+                        <h2>No movies bruh</h2>
+                    </div>
+                )}            
+        </div>
+    )
 }
 
 export default App;
